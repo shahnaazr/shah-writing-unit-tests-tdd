4b3934f52e3cf1d19390d41fbab8d3fff47adc05
Shahnaaz Rahamatullah <shahnaaz.rahamatullah@jugo.io>
2023-10-30 11:45:21 +0000

    refactor: modify highestNumberAsString function

--- demo.js
+++ demo.js
@@ -1,4 +1,3 @@
 export const highestNumberAsString = (arrayOfNumbers) => {
-  const max = Math.max(...arrayOfNumbers);
-  return max.toString();
+  return Math.max(...arrayOfNumbers).toString();
 };
